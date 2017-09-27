9ad198fc841431914f54ea823ff000d2cdae6226
Woongsup Jung <woongsup123@hotmail.com>
2017-09-27 15:25:48 +0900

    Initial commit

---
+++ assignment.py
@@ -0,0 +1,12 @@
+a = 1
+b = a + 1
+print (a, b, sep=':')
+
+c, d = 3, 6
+print (c, d)
+
+c, d = d, c
+print (c, d)
+
+e = 'hi'
+print(e)
---
+++ variables.py
@@ -0,0 +1,10 @@
+# 변수 이름은 문자, 숫자, _ 로 구성해야 한다.
+import keyword
+
+friend = 1
+a = 10
+my_name = '정웅섭'
+_yourName = '둘리'
+member1 = '도우넛'
+
+print(keyword.kwlist)
